ec58480e4234edda7a502ca3941f88ce2ff3c39b
Praise Ating James <praisejames789@gmail.com>
2025-12-06 20:20:36 +0100

    New  commit: Project changes

--- Backend Development/intro.js
+++ Backend Development/intro.js
@@ -1,3 +1,7 @@
-console.log("Name:[Praise Ating James]");
-console.log("Age:[18]");
-console.log("Favourite Language:[Python]");
+const name = "Praise Ating James";
+const age = 19;
+const favouritelanguage = "Python";
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+console.log("My favourite programming language is " + favouritelanguage);
